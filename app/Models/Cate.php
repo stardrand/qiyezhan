862d0e95e3c840a1f187bf7864f4625b01120383
star <3030173250@qq.com>
2020-06-22 14:51:05 +0800

    add1

---
+++ app/Models/Cate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cate extends Model
+{
+    //
+    protected  $table='cate';
+    protected $primaryKey='id';
+    public $timestamps=false;
+}
